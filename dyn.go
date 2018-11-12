43a46f3a6ea951dfda1c4117a6045d43321cf6e4
Loh Siu Yin <siuyin@beyondbroadcast.com>
2018-11-12 14:43:54 +0000

    Added gzip compression.

--- dyn.go
+++ dyn.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"compress/gzip"
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -10,21 +14,34 @@ const (
 	dynHead = `<!DOCTYPE html>
 <html>
 <head>
-<title>{{ .Title }}</title>
-<link href="/css/pure-min.css" rel="stylesheet">
-<link href="/css/app.css" rel="stylesheet">
+	<title>{{ .Title }}</title>
+	<link href="/css/pure-min.css" rel="stylesheet">
+	<link href="/css/app.css" rel="stylesheet">
 </head>
-<header>The Header</header>
 <body>
+	<div class="flex-row">
+		<nav class="pure-menu">
+			<ul class="pure-menu-list">
+				<li><a href="/" class="pure-menu-link">Home</a></li>
+				<li><a href="/grid.html" class="pure-menu-link">Grid Layout</a></li>
+				<li><a href="/flex.html" class="pure-menu-link">Flex Layout</a></li>
+				<li class="pure-menu-item pure-menu-disabled">Dynamic</li>
+			</ul>
+		</nav>
+		<section>
+			<header>The Header</header>
 `
 
-	dynBody = `<h1>Hello</h1>
-<p>received: {{ .Path }}
+	dynBody = `	<h1>Hello</h1>
+			<p>received: {{ .Path }}
+			<p>headers: {{ .Headers }}
 `
 	dynFoot = `
-<footer>Copyright &copy {{ .Year }} Beyond Broadcast LLP.
-	All rights reserved.
-</footer>
+			<footer>Copyright &copy {{ .Year }} Beyond Broadcast LLP.
+				All rights reserved.
+			</footer>
+		</section>
+	</div>
 </body>
 </html>`
 )
@@ -39,14 +56,28 @@ func init() {
 
 type dynHdlr struct{}
 
-func (h *dynHdlr) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *dynHdlr) ServeHTTP(wr http.ResponseWriter, r *http.Request) {
+	hdrs := strings.Join(r.Header["Accept-Encoding"], "|")
+	var (
+		w  io.Writer
+		zw *gzip.Writer
+	)
+	w = wr
+	if strings.Index(hdrs, "gzip") != -1 {
+		zw = gzip.NewWriter(wr)
+		defer zw.Close()
+		wr.Header().Set("Content-Type", "text/html; charset=utf-8")
+		wr.Header().Set("Content-Encoding", "gzip")
+		w = zw
+	}
 	dHeadT.Execute(w, struct {
 		Title string
 	}{"dyn1"})
 
 	dBodyT.Execute(w, struct {
-		Path string
-	}{r.URL.Path})
+		Path    string
+		Headers string
+	}{r.URL.Path, fmt.Sprintf("%s", r.Header)})
 
 	dFootT.Execute(w, struct {
 		Year string
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"compress/gzip"
 	"fmt"
 	"io"
 	"log"
@@ -39,6 +40,44 @@ func main() {
 			log.Printf("upload copy file: %v", err)
 		}
 	})
-	http.Handle("/", http.FileServer(http.Dir("static")))
+
+	http.Handle("/", gzHandler(http.FileServer(http.Dir("static"))))
 	log.Fatal(http.ListenAndServeTLS(":8080", "server.pem", "server-key.pem", nil))
+}
+
+type zh struct {
+	h http.Handler
+}
+
+func (z *zh) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Encoding", "gzip")
+	z.h.ServeHTTP(gzRW(w), r)
+}
+func gzHandler(h http.Handler) http.Handler {
+	z := &zh{h: h}
+	return z
+}
+
+type zrw struct {
+	in http.ResponseWriter
+}
+
+func (w *zrw) Header() http.Header {
+	return w.in.Header()
+}
+func (w *zrw) Write(b []byte) (int, error) {
+	zw := gzip.NewWriter(w.in)
+	i, err := zw.Write(b)
+	if err != nil {
+		return i, err
+	}
+	defer zw.Close()
+	return i, nil
+}
+func (w *zrw) WriteHeader(status int) {
+	w.in.WriteHeader(status)
+}
+func gzRW(w http.ResponseWriter) http.ResponseWriter {
+	zw := &zrw{in: w}
+	return zw
 }
